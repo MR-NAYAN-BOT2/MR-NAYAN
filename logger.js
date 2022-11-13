27471abb6924751199f3d29dee6533d9d02df0d9
MR-NAYAN-BOT2 <118095867+MR-NAYAN-BOT2@users.noreply.github.com>
2022-11-13 16:19:35 +0600

    Update logger.js

--- logger.js
+++ logger.js
@@ -2,15 +2,15 @@ const chalk = require('chalk');
 //const notifier = require('node-notifier');
 
 module.exports = (str, end) => {
-	console.log(chalk.hex('#9900FF').bold(`${end || '[ FCA-HZI ]'} > `) + str);
+	console.log(chalk.hex('#9900FF').bold(`${end || '[ MR.NAYAN ]'} > `) + str);
 };
 module.exports.onLogger = (str,end,ctscolor) => { 
 	var checkbutdak = ctscolor.replace("#",'');
 	if (ctscolor.indexOf('#') != 1) {
-		console.log(chalk.hex('#00CCCC').bold(`${end || '[ FCA-HZI ]'} > `) + str);
+		console.log(chalk.hex('#00CCCC').bold(`${end || '[ MR.NAYAN ]'} > `) + str);
 	}
 	else if (!isNaN(checkbutdak)) {
-		console.log(chalk.hex(ctscolor).bold(`${end || '[ FCA-HZI ]'} > `) + str);
+		console.log(chalk.hex(ctscolor).bold(`${end || '[ MR.NAYAN ]'} > `) + str);
 	} 
-	else console.log(chalk.hex('#00CCCC').bold(`${end || '[ FCA-HZI ]'} > `) + str);
+	else console.log(chalk.hex('#00CCCC').bold(`${end || '[ MR.NAYAN ]'} > `) + str);
 }
